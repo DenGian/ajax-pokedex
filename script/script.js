a75916e72080339230f633a0155deed5344fbf60
DenGian <97441878+DenGian@users.noreply.github.com>
2022-05-24 15:08:16 +0200

    added moveset for pokemons

--- script/script.js
+++ script/script.js
@@ -5,8 +5,6 @@
 }
 loadPokemon() */
 
-
-
 document.getElementById('search').addEventListener('click',async function(){ // To get input from user
     let feedback = document.getElementById('inputField').value; //input user itself
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${feedback}`) // ${}, see readme -> useful resources // to fetch data from API
@@ -17,5 +15,9 @@ document.getElementById('search').addEventListener('click',async function(){ // 
     document.getElementById('shinyM').src = data.sprites.other.home.front_shiny; // front image shiny male
     document.getElementById('frontF').src = data.sprites.other.home.front_female; // front image female
     document.getElementById('shinyF').src = data.sprites.other.home.front_shiny_female; // front image shiny female
+    document.getElementById('moves').innerHTML = data.moves.map(move => move.move.name).slice(0, 5).join('_ ') // get span to display moves, fetch info form API, .slice-> (start#,end#), .join acts as separator
+    if(feedback === '132' || feedback === 'ditto'){
+        document.getElementById('moves').innerHTML = data.moves[0].move.name;
+    }
 })
 
